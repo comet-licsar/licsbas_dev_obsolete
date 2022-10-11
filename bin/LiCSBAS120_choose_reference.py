55bc042274fdb221269ef5fe05b757479f93512f
qi-ou <ouqi1005@gmail.com>
2022-10-11 22:26:54 +0100

    new script for choosing reference

---
+++ bin/LiCSBAS120_choose_reference.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import argparse
+import LiCSBAS_io_lib as io_lib
+import LiCSBAS_tools_lib as tools_lib
+
+def block_sum(arra1, k):
+    result = np.add.reduceat(np.add.reduceat(arra1, np.arange(0, arra1.shape[0], k), axis=0),
+                             np.arange(0, arra1.shape[1], k), axis=1)
+    return result
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Detect coregistration error")
+    parser.add_argument('-f', "--frame_dir", default="./", help="directory of LiCSBAS output of a particular frame")
+    parser.add_argument('-g', '--GEOCml_dir', dest="unw_dir", default="GEOCml10GACOS", help="folder containing unw input")
+    parser.add_argument('-t', '--ts_dir', dest="ts_dir", default="TS_GEOCml10GACOS", help="folder containing time series")
+    parser.add_argument('-w', '--window_size', dest="win", default="5", help="Window size in km")
+    args = parser.parse_args()
+
+    ifgdir = os.path.abspath(os.path.join(args.frame_dir, args.unw_dir))
+    tsadir = os.path.abspath(os.path.join(args.frame_dir, args.ts_dir))
+    resultsdir = os.path.join(tsadir, 'results')
+    infodir = os.path.join(tsadir, 'info')
+
+    ### Get size
+    mlipar = os.path.join(ifgdir, 'slc.mli.par')
+    width = int(io_lib.get_param_par(mlipar, 'range_samples'))
+    length = int(io_lib.get_param_par(mlipar, 'azimuth_lines'))
+    print("\nSize         : {} x {}".format(width, length), flush=True)
+
+    ### Get resolution
+    dempar = os.path.join(ifgdir, 'EQA.dem_par')
+    lattitude_resolution = float(io_lib.get_param_par(dempar, 'post_lat'))
+    window_size = int(args.win / 110 / lattitude_resolution)   # 110 km per degree latitude
+
+    ifgdates = tools_lib.get_ifgdates(ifgdir)
+
+    n_unw = np.zeros((length, width), dtype=np.float32)
+    n_coh = np.zeros((length, width), dtype=np.float32)
+    n_con = np.zeros((length, width), dtype=np.float32)
+
+    for ifgd in ifgdates:
+        # turn ifg into ones and zeros for non-nan and nan values
+        unwfile = os.path.join(ifgdir, ifgd, ifgd+'.unw')
+        unw = io_lib.read_img(unwfile, length, width)
+        unw[unw != 0] = 1  # turn non-nan pixels into 1, keep nan pixels as 0
+        n_unw += unw   # stacking unw pixels
+
+        # coherence values from 0 to 1
+        ccfile = os.path.join(ifgdir, ifgd, ifgd + '.cc')
+        coh = io_lib.read_img(ccfile, length, width, np.uint8)
+        coh = coh.astype(np.float32) / 255  # keep 0 as 0 which represent nan values
+        n_coh += coh
+
+        # connected components in terms of component area (pixel count)
+        confile = os.path.join(ifgdir, ifgd, ifgd+'.conncomp')
+        con = io_lib.read_img(confile, length, width)
+        # replace component index by component size. the first component is the 0th component, which should be of size 0
+        uniq_components, pixel_counts = np.unique(con.flatten(), return_counts=True)[1:]
+        for i, component in enumerate(uniq_components[1:]):
+            con[con==component] = pixel_counts[i+1]
+        n_con += con
+
+        del unw, coh, con
+
+    block_unw = block_sum(n_unw, window_size)
+    block_coh = block_sum(n_coh, window_size)
+    block_con = block_sum(n_con, window_size)
+    # block_unw = block_unw / np.max(block_unw)
+    # block_coh = block_coh / np.max(block_coh)
+    # block_con = block_con / np.max(block_con)
+    fig, ax = plt.subplots(1, 3)
+    im_unw = ax[0].imshow(block_unw)
+    im_coh = ax[1].imshow(block_coh)
+    im_con = ax[2].imshow(block_con)
+    plt.colorbar(im_unw, ax=ax[0])
+    plt.colorbar(im_coh, ax=ax[1])
+    plt.colorbar(im_con, ax=ax[2])
+    fig.savefig("reference.png", dpi=300, bbox_inches='tight')
+
+
+
